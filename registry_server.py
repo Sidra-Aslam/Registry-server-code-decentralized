8ccc09bb98d5669dc08a8c2d6375eb36e40b9613
Michael Mrissa <michael.mrissa@innorenew.eu>
2021-02-02 09:28:00 +0100

    registry server optimized

--- registry_server.py
+++ registry_server.py
@@ -24,8 +24,27 @@ def peers():
         # get address (api url) of new client from request
         client_address = request.get_json()["client_address"]
         headers = {'Content-Type': "application/json"}
+
+       #if client is in the list it is a disconnect
+        if client_address in peer_list:
+            # register new peer/client on registry server
+            peer_list.remove(client_address)
+            # iterate all existing peers to acknowledge new client
+            for peer in peer_list:
+                try:
+                    # send request to other peers to acknowledge new client
+                    requests.post(peer + "/peers", data=json.dumps(list(peer_list)), headers=headers)
+                except:
+                    # if there is any error in communication then remove that peer from the peers list
+                    remove_peers.remove(peer)
+            
+            # remove not responding peers from main list
+            for peer in remove_peers:
+                peer_list.remove(peer)
+             
+
         # check if client address is not already in the peers list
-        if client_address not in peer_list:
+        else:
             # register new peer/client on registry server
             peer_list.add(client_address)
 
@@ -42,23 +61,7 @@ def peers():
             for peer in remove_peers:
                 peer_list.remove(peer)
             
-        #if client is in the list it is a disconnect
-        elif client_address in peer_list:
-            # register new peer/client on registry server
-            peer_list.remove(client_address)
-            # iterate all existing peers to acknowledge new client
-            for peer in peer_list:
-                try:
-                    # send request to other peers to acknowledge new client
-                    requests.post(peer + "/peers", data=json.dumps(list(peer_list)), headers=headers)
-                except:
-                    # if there is any error in communication then remove that peer from the peers list
-                    remove_peers.remove(peer)
-            
-            # remove not responding peers from main list
-            for peer in remove_peers:
-                peer_list.remove(peer)
-            
+ 
     print('Current peer list')
     print(peer_list)
     
