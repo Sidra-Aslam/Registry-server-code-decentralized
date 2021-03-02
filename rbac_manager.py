f542e142f7e338f3c2751f0d42e967ba9816b022
Sidra Aslam <sidra.aslam@innorenew.eu>
2021-03-02 21:46:43 +0100

    combine endpoints

--- rbac_manager.py
+++ rbac_manager.py
@@ -28,7 +28,7 @@ class RbacManager:
         self.acl.allow('business_partner', 'read', 'blockchain')
         self.acl.allow('public_user', 'read', 'blockchain')
         
-    # this method will be used to verify permission
+    # this method will be used to verify permission of actions (delete,write, update, read)
     def verify_permission(self, role, operation, resource):
         start_time = time.time()
         result = self.acl.is_allowed(role, operation, resource) is not None
@@ -38,3 +38,13 @@ class RbacManager:
     # this method will be used to authenticate role if it exists
     def authenticate(self, client, role):
         return client in self.clients and self.acl._roles.get(role) is not None
+
+    # check if current role is valid to access block based on privacy type e.g user is allow to access public data only 
+    def verify_privacy(self, client_role, privacy_type):
+        if( (client_role == 'owner') or
+            (client_role == 'business_partner' and privacy_type != 'private-data') or
+            (client_role == 'public_user' and privacy_type == 'public-data')):
+            print('Privacy validated')
+            return True
+        else:
+            return False
